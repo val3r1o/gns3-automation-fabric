1d94f65952b98cdd458a7bee821e5abba88e73ce
Valerio Martini <valerio.martini@gmail.com>
2021-11-18 17:20:42 +0100

    Update pizza.sh

--- pizza.sh
+++ pizza.sh
@@ -11,7 +11,12 @@ sudo apt install gns3-gui gns3-server
 sudo apt-get install konsole
 
 wget https://www.dropbox.com/s/u29uly3b3vphyi6/evpn-simple-fabric.gns3project
+mkdir /home/valerio/GNS3/projects/evpn-simple-fabric
+sudo mv evpn-simple-fabric.gns3project /home/valerio/GNS3/projects/evpn-simple-fabric/evpn-simple-fabric.gns3
 
-sudo mv evpn-simple-fabric.gns3project /home/XXXyournamexxx/GNS3/projects/evpn-simple-fabric/evpn-simple-fabric.gns3project
-
-gns3
+echo ------------------------------------------------------------------------------------------------------
+echo ------------------ This is an automatic script -------------------------------------------------------
+echo ------------------------------------------------------------------------------------------------------
+echo ------------------------------------------------- Dell NETWORKING ------------------------------------
+echo --------------------------valerio.martini@gmail.com---------------------------------------------------
+echo ------------------------------------------------------------------------------------------------------

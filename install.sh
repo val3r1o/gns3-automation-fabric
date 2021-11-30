2858026bdb60ef563d09e23022ecb2bd916ec5c2
Valerio Martini <valerio.martini@gmail.com>
2021-11-30 10:29:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ sudo apt-get install konsole
 # fix oob port and ubuntu all-in-one reachability
 wget https://www.dropbox.com/s/1dl86jern3g5zrb/simple-leaf-ubuntuhost.gns3project
 
+#virbr0 should have been created by GNS3 installation. change IP address and down/up
+sudo ifconfig virbr0 down
+sudo ifconfig virbr0 10.10.10.1 netmask 255.255.255.0 up
+
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------ This is an automatic script -------------------------------------------------------
 echo ------------------------------------------------------------------------------------------------------

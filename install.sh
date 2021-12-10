42c66400df2f1987dc5b0a280cdeb2e931e30cf4
Valerio Martini <valerio.martini@gmail.com>
2021-12-10 17:57:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 
 # 
-sudo apt upgrade
-sudo apt install net-tools
-sudo apt install openssh-server
+sudo apt -y upgrade
+sudo apt -y install net-tools
+sudo apt -y install openssh-server
 # sudo service ssh status
 # ref: https://docs.gns3.com/docs/getting-started/installation/linux 
 sudo add-apt-repository ppa:gns3/ppa                             
-sudo apt install gns3-gui gns3-server
-sudo apt-get install konsole
+sudo apt -y install gns3-gui gns3-server
+sudo apt -y install konsole
 
 # fix oob port and ubuntu all-in-one reachability
 wget https://www.dropbox.com/s/1dl86jern3g5zrb/simple-leaf-ubuntuhost.gns3project

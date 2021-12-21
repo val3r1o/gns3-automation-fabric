a92f7da27e2633fe513f703cf1d7f0a92e9feb3c
Valerio Martini <valerio.martini@gmail.com>
2021-12-21 10:56:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt -y install gns3-gui gns3-server
 sudo apt -y install konsole
 
-# fix oob port and ubuntu all-in-one reachability
-wget https://www.dropbox.com/s/1dl86jern3g5zrb/simple-leaf-ubuntuhost.gns3project
-
-#virbr0 should have been created by GNS3 installation. change IP address to align internal IP's
+#virbr0 should have been created by GNS3 installation. This change IP address making a new config file
 sudo mv default.xml /etc/libvirt/qemu/networks/
+
+# download an example project
+wget https://www.dropbox.com/s/1dl86jern3g5zrb/simple-leaf-ubuntuhost.gns3project
 
 echo ------------------------------------------------------------------------------------------------------
 echo ------------------ This is an automatic script -------------------------------------------------------
